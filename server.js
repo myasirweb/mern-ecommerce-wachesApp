c7279df9fe831b1568cd650528d660031f811bf6
Muhammad Yasir <119052144+myasirweb@users.noreply.github.com>
2023-12-17 22:09:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors';
-import path from 'path'
+import path from 'path';
+import { fileURLToPath} from 'url';
 
 
 //configure env 
@@ -14,9 +15,13 @@ dotenv.config();
 
 //..rest object..//
 const app = express();
-//database config
 
+//database config
 connectDB();
+
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 ///middelwares
 app.use(cors());
